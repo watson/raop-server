1312b2ebecd134c14181d7317be71700a5ad4810
Thomas Watson Steen <w@tson.dk>
2014-07-26 21:52:35 +0200

    Add ways to configure the RAOP server

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-raop-server
-===========
+# raop-server
 
-A simple RAOP server
+An RAOP server
+
+## Installation
+
+```
+npm install raop-server
+```
+
+## Example usage
+
+```javascript
+var raop = require('raop-server')('My RAOP Server');
+
+raop.on('request', function (c) {
+  // do your stuff
+});
+
+raop.listen(7000); // start server on port 7000
+```
+
+## API
+
+### Constructor
+
+Get the constructor by requireing the `raop-server` node module and call
+it. It takes the optional arguments `name`, `options` and `onRequest`.
+Either of them can be left out, so calling with only `options` or only
+`name` and `onRequest` is ok:
+
+```javascript
+require('raop-server')(name, options, onRequest);
+```
+
+Constructor arguments:
+
+- `name` - name of the AirPlay server (default: 'Node.js')
+- `options` - options object (default: {})
+- `onRequest` - callback function called upon each request (default: No listener is added. Remember to manually listen on the `request` event)
+
+Options:
+
+- `txt` - An object used to override the TXT record
+
+### Server
+
+The constructor returns a basic Node.js TCP server, so remember to call
+`.listen()` and optionally add a `request` event listener if one hasn't
+been provided as an argument to the constructor.
+
+## License
+
+MIT
--- index.js
+++ index.js
@@ -5,45 +5,62 @@ var mdns = require('mdns');
 var getmac = require('getmac');
 var debug = require('debug')('raop');
 
-module.exports = function (onRequest) {
+var pkg = require('./package.json');
+
+module.exports = function (name, options, onRequest) {
+  if (typeof name === 'object') {
+    options = name;
+    name = undefined;
+  } else if (typeof name === 'function') {
+    onRequest = name;
+    name = undefined;
+  } else if (typeof options === 'function') {
+    onRequest = options;
+    options = undefined;
+  }
+  if (!options) options = {};
+  if (!options.txt) options.txt = {};
+  if (!options.name) options.name = name || 'Node.js';
+
+  var start = function () {
+    debug('Getting server MAC address');
+    getmac.getMac(function (err, mac) {
+      if (err) throw err;
+
+      var port = server.address().port;
+      var name = mac.toUpperCase().replace(/:/g, '') + '@' + options.name;
+      var model = 'NodeAirPlay' + pkg.version.split('.').slice(0,-1).join(',');
+
+      var mdnsOpt = {
+        name: name,
+        txtRecord: {
+          txtvers: options.txt.txtvers || '1', // TXT record version 1
+          ch: options.txt.ch || '2',           // audio channels: stereo
+          cn: options.txt.cn || '0,1',         // audio codecs
+          ek: options.txt.ek || '1',           // ?
+          et: options.txt.et || '0,1',         // supported encryption types
+          sv: options.txt.sv || 'false',       // ?
+          da: options.txt.da || 'true',        // ?
+          sr: options.txt.sr || '44100',       // audio sample rate: e.g. 44100 Hz
+          ss: options.txt.ss || '16',          // audio sample size: e.g. 16-bit
+          pw: options.txt.pw || 'false',       // does the speaker require a password?
+          vn: options.txt.vn || '65537',       // ?
+          tp: options.txt.tp || 'TCP,UDP',     // supported transport: TCP or UDP
+          vs: options.txt.vs || pkg.version,   // server version
+          am: options.txt.am || model,         // device model
+          fv: options.txt.fv || '76400.10',    // ?
+          md: options.txt.md || '0,1,2',       // supported metadata types
+          sf: options.txt.sf || '0x0'          // ?
+        }
+      };
+
+      debug('Starting server with name %s...', options.name);
+      var ad = mdns.createAdvertisement(mdns.tcp('raop'), port, mdnsOpt);
+      ad.start();
+    });
+  };
+
   var server = net.createServer(onRequest);
-  server.on('listening', function () {
-    start(server.address().port);
-  });
+  server.on('listening', start);
   return server;
-};
-
-var start = function (port) {
-  debug('Getting server MAC address');
-  getmac.getMac(function (err, mac) {
-    if (err) throw err;
-
-    mac = mac.toUpperCase().replace(/:/g, '');
-
-    var options = {
-      name: mac + '@Node.js',
-      txtRecord: {
-        txtvers: '1',       // TXT record version 1
-        ch: '2',            // audio channels: stereo
-        cn: '0,1',          // audio codecs
-        ek: '1',            // ?
-        et: '0,1',          // supported encryption types
-        sv: 'false',        // ?
-        da: 'true',         // ?
-        sr: '44100',        // audio sample rate: e.g. 44100 Hz
-        ss: '16',           // audio sample size: e.g. 16-bit
-        pw: 'false',        // does the speaker require a password?
-        vn: '65537',        // ?
-        tp: 'TCP,UDP',      // supported transport: TCP or UDP
-        vs: '105.1',        // server version
-        am: 'AirPort4,107', // device model
-        fv: '76400.10',     // ?
-        sf: '0x0'           // ?
-      }
-    };
-
-    debug('Starting server with name %s...', options.name);
-    var ad = mdns.createAdvertisement(mdns.tcp('raop'), port, options);
-    ad.start();
-  });
 };
