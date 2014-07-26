95192a64fb85c5e70bd1412edd7f219edd37a4e9
Thomas Watson Steen <w@tson.dk>
2014-07-27 00:46:30 +0200

    Mimic an AppleTV3,1 with firmware 200.54

--- README.md
+++ README.md
@@ -25,23 +25,19 @@ raop.listen(7000); // start server on port 7000
 ### Constructor
 
 Get the constructor by requireing the `raop-server` node module and call
-it. It takes the optional arguments `name`, `options` and `onRequest`.
-Either of them can be left out, so calling with only `options` or only
+it. It takes the optional arguments `name`, `txtRecord` and `onRequest`.
+Either of them can be left out, so calling with only `txtRecord` or only
 `name` and `onRequest` is ok:
 
 ```javascript
-require('raop-server')(name, options, onRequest);
+require('raop-server')(name, txtRecord, onRequest);
 ```
 
 Constructor arguments:
 
 - `name` - name of the AirPlay server (default: 'Node.js')
-- `options` - options object (default: {})
+- `txtRecord` - object used to replace stock TXT record
 - `onRequest` - callback function called upon each request (default: No listener is added. Remember to manually listen on the `request` event)
-
-Options:
-
-- `txt` - An object used to override the TXT record
 
 ### Server
 
--- index.js
+++ index.js
@@ -7,20 +7,18 @@ var debug = require('debug')('raop');
 
 var pkg = require('./package.json');
 
-module.exports = function (name, options, onRequest) {
+module.exports = function (name, txtRecord, onRequest) {
   if (typeof name === 'object') {
     options = name;
     name = undefined;
   } else if (typeof name === 'function') {
     onRequest = name;
     name = undefined;
-  } else if (typeof options === 'function') {
-    onRequest = options;
-    options = undefined;
+  } else if (typeof txtRecord === 'function') {
+    onRequest = txtRecord;
+    txtRecord = undefined;
   }
-  if (!options) options = {};
-  if (!options.txt) options.txt = {};
-  if (!options.name) options.name = name || 'Node.js';
+  if (!name) name = 'Node.js';
 
   var start = function () {
     debug('Getting server MAC address');
@@ -28,35 +26,29 @@ module.exports = function (name, options, onRequest) {
       if (err) throw err;
 
       var port = server.address().port;
-      var name = mac.toUpperCase().replace(/:/g, '') + '@' + options.name;
       var model = 'NodeAirPlay' + pkg.version.split('.').slice(0,-1).join(',');
 
-      var mdnsOpt = {
-        name: name,
-        txtRecord: {
-          txtvers: options.txt.txtvers || '1', // TXT record version 1
-          ch: options.txt.ch || '2',           // audio channels: stereo
-          cn: options.txt.cn || '0,1',         // audio codecs
-          ek: options.txt.ek || '1',           // ?
-          et: options.txt.et || '0,1',         // supported encryption types
-          sv: options.txt.sv || 'false',       // ?
-          da: options.txt.da || 'true',        // ?
-          sr: options.txt.sr || '44100',       // audio sample rate: e.g. 44100 Hz
-          ss: options.txt.ss || '16',          // audio sample size: e.g. 16-bit
-          pw: options.txt.pw || 'false',       // does the speaker require a password?
-          vn: options.txt.vn || '65537',       // ?
-          tp: options.txt.tp || 'TCP,UDP',     // supported transport: TCP or UDP
-          vs: options.txt.vs || pkg.version,   // server version
-          am: options.txt.am || model,         // device model
-          fv: options.txt.fv || '76400.10',    // ?
-          md: options.txt.md || '0,1,2',       // supported metadata types
-          sf: options.txt.sf || '0x0'          // ?
-        }
+      txtRecord = txtRecord || {
+        cn: '0,1,2,3',        // audio codecs
+        da: 'true',
+        et: '0,3,5',          // supported encryption types
+        ft: '0x5A7FFFF7,0xE', // ? seems to be the same as 'features' in AirPlay
+        md: '0,1,2',          // supported metadata types
+        am: model,            // device model
+        sf: '0x44',           // ? seems to be the same as 'flags' in AirPlay
+        tp: 'UDP',            // supported transport
+        vn: '65537',
+        vs: pkg.version,      // server version
+        vv: '2'               // ? maybe the txt record format version
       };
 
-      debug('Starting server with name %s...', options.name);
-      var ad = mdns.createAdvertisement(mdns.tcp('raop'), port, mdnsOpt);
-      ad.start();
+      debug('Starting server with name %s...', name);
+      mdns
+        .createAdvertisement(mdns.tcp('raop'), port, {
+          name: mac.toUpperCase().replace(/:/g, '') + '@' + name,
+          txtRecord: txtRecord
+        })
+        .start();
     });
   };
 
