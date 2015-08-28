a67c24ac47e14485242a8ce217bd5d31b00c3bd8
Thomas Watson Steen <w@tson.dk>
2015-08-28 19:09:19 +0200

    Refactor bulk of code

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var mdns = require('mdns')
 var getmac = require('getmac')
 var debug = require('debug')('raop')
 
-var pkg = require('./package')
-
 var raop = module.exports = function (name, opts, onRequest) {
   if (!name) name = 'Node.js'
   if (!opts) opts = {}
@@ -14,36 +12,48 @@ var raop = module.exports = function (name, opts, onRequest) {
   if (typeof name === 'function') return raop(null, null, name)
   if (typeof opts === 'function') return raop(name, null, opts)
 
-  var start = function () {
+  var txt = opts.txt
+
+  if (!txt) {
+    var pkg = require('./package')
+    var model = 'NodeAirPlay' + pkg.version.split('.').slice(0, -1).join(',')
+    txt = {
+      txtvers: '1',    // TXT record version 1
+      ch: '2',         // audio channels: stereo
+      cn: '0,1,2,3',   // audio codecs
+      et: '0,3,5',     // supported encryption types
+      md: '0,1,2',     // supported metadata types
+      pw: 'false',     // does the speaker require a password?
+      sr: '44100',     // audio sample rate: 44100 Hz
+      ss: '16',        // audio sample size: 16-bit
+      tp: 'UDP',       // supported transport: TCP or UDP
+      vs: pkg.version, // server version
+      am: model        // device model
+    }
+  }
+
+  var broadcast = function (mac) {
+    var port = server.address().port
+    debug('Starting server with name %s...', name)
+    mdns
+      .createAdvertisement(mdns.tcp('raop'), port, {
+        name: mac.toUpperCase().replace(/:/g, '') + '@' + name,
+        txtRecord: txt
+      })
+      .start()
+  }
+
+  var getMac = function (cb) {
     debug('Getting server MAC address')
     getmac.getMac(function (err, mac) {
       if (err) throw err
-
-      var port = server.address().port
-      var model = 'NodeAirPlay' + pkg.version.split('.').slice(0, -1).join(',')
-
-      var txt = opts.txt || {
-        txtvers: '1',    // TXT record version 1
-        ch: '2',         // audio channels: stereo
-        cn: '0,1,2,3',   // audio codecs
-        et: '0,3,5',     // supported encryption types
-        md: '0,1,2',     // supported metadata types
-        pw: 'false',     // does the speaker require a password?
-        sr: '44100',     // audio sample rate: 44100 Hz
-        ss: '16',        // audio sample size: 16-bit
-        tp: 'UDP',       // supported transport: TCP or UDP
-        vs: pkg.version, // server version
-        am: model        // device model
-      }
-
-      debug('Starting server with name %s...', name)
-      mdns
-        .createAdvertisement(mdns.tcp('raop'), port, {
-          name: (opts.mac || mac).toUpperCase().replace(/:/g, '') + '@' + name,
-          txtRecord: txt
-        })
-        .start()
+      cb(mac)
     })
+  }
+
+  var start = function () {
+    if (opts.mac) broadcast(opts.mac)
+    else getMac(broadcast)
   }
 
   var server = net.createServer(onRequest)
