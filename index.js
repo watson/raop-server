84a2b4f21f83c9ad66b8dec1e167e607d9367a5c
Thomas Watson Steen <w@tson.dk>
2015-09-06 01:46:59 +0200

    Refactor to use raop-mdns-server

--- index.js
+++ index.js
@@ -1,61 +1,28 @@
 'use strict'
 
 var net = require('net')
-var mdns = require('mdns')
-var getmac = require('getmac')
-var debug = require('debug')('raop')
+var mdns = require('raop-mdns-server')
+var xtend = require('xtend')
+var pkg = require('./package')
+var debug = require('debug')(pkg.name)
 
-var raop = module.exports = function (name, opts, onRequest) {
-  if (!name) name = 'Node.js'
+var raop = module.exports = function (opts, onRequest) {
+  if (typeof opts === 'function') return raop(null, opts)
+  if (typeof opts === 'string') return raop({ name: opts }, onRequest)
   if (!opts) opts = {}
-  if (typeof name === 'object') return raop(null, name, opts)
-  if (typeof name === 'function') return raop(null, null, name)
-  if (typeof opts === 'function') return raop(name, null, opts)
-
-  var txt = opts.txt
-
-  if (!txt) {
-    var pkg = require('./package')
-    var model = 'NodeAirPlay' + pkg.version.split('.').slice(0, -1).join(',')
-    txt = {
-      txtvers: '1',    // TXT record version 1
-      ch: '2',         // audio channels: stereo
-      cn: '0,1,2,3',   // audio codecs
-      et: '0,3,5',     // supported encryption types
-      md: '0,1,2',     // supported metadata types
-      pw: 'false',     // does the speaker require a password?
-      sr: '44100',     // audio sample rate: 44100 Hz
-      ss: '16',        // audio sample size: 16-bit
-      tp: 'UDP',       // supported transport: TCP or UDP
-      vs: pkg.version, // server version
-      am: model        // device model
-    }
-  }
-
-  var broadcast = function (mac) {
-    var port = server.address().port
-    var fullName = mac.toUpperCase().replace(/:/g, '') + '@' + name
-    debug('Starting server %s - port %d...', fullName, port)
-    mdns
-      .createAdvertisement(mdns.tcp('raop'), port, { name: fullName, txtRecord: txt })
-      .start()
-  }
-
-  var getMac = function (cb) {
-    debug('Getting server MAC address')
-    getmac.getMac(function (err, mac) {
-      if (err) return server.emit('error', err)
-      debug('Found MAC address', mac)
-      cb(mac)
-    })
-  }
-
-  var start = function () {
-    if (opts.mac) broadcast(opts.mac)
-    else getMac(broadcast)
-  }
 
   var server = net.createServer(onRequest)
-  server.on('listening', start)
+
+  server.on('listening', function () {
+    var port = server.address().port
+    debug('Listening on port %d', port)
+
+    opts = xtend({ name: 'Node.js', version: pkg.version, port: port }, opts)
+
+    mdns(opts, function (err) {
+      if (err) server.emit('error', err)
+    })
+  })
+
   return server
 }
